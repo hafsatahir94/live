bed953c7068ddead561a66d6a22a05860d203d3c
hafsatahir94 <hafsafazal94@hotmail.com>
2025-01-10 12:52:10 +0500

    done

---
+++ app.js
@@ -0,0 +1,16 @@
+var cityNames = ["karachi", "faislabad", "lahore"]
+
+// console.log (cityNames [1])
+// console.log(cityNames.length);
+// cityNames[2] ="malamjabba"
+// console.table(cityNames) 
+// console.log(cityNames.length)
+// console.log("hello how are " + cityNames[1]);
+// console.log(cityNames)
+// cityNames[6] = "balakot"
+// cityNames[5] ="lime"
+console.log(cityNames.pop());
+// console.log (cityNames.pop());
+
+console.table(cityNames.unshift("kashmir" , "malamjabba"));
+console.table(cityNames.push("eee" , "kkkk"));
